83352c3b4326c38d2572ae31343477249fa548e4
jli <jeffrey.li@teamaol.com>
2016-05-06 16:31:32 -0400

    no message

---
+++ index.js
@@ -0,0 +1,13 @@
+bower install
+gulp
+
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function(request, response) {
+//   response.render('dist/');
+// });
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
